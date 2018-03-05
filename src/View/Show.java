a2610267a7d61627d7a651b91e5d39d5842457f2
AliceMcClain <36777803+AliceMcClain@users.noreply.github.com>
2018-03-04 20:02:30 -0700

    Add functinality to displayCleanCharacterSheet

--- src/View/Show.java
+++ src/View/Show.java
@@ -2,6 +2,8 @@ package View;
 
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -25,19 +27,99 @@ public class Show
 	
 	//Initializes the Display of a fresh character sheet for new Character entry;
 	// returns the character created by the field
+	//(TextFields are defaulted at 150 length and 25 height)
 	public static void displayCleanCharacterSheet(Stage primaryStage)
 	{
+		ImageView viewSheet = new ImageView();
+		viewSheet.setImage(new Image("file:Character Sheet (Official) - Copy_Page_1.png",790,790,true,true));
+		
 		TextField characterNameField = new TextField();
-		characterNameField.setPromptText("Character Name");
-		characterNameField.setLayoutY(10);
+		characterNameField.setPromptText("Name");
+		characterNameField.setLayoutY(59);
+		characterNameField.setLayoutX(50);
 		
 		TextField characterRaceField = new TextField();
-		characterRaceField.setPromptText("Character Race");
-		characterRaceField.setLayoutX(150);
-		characterRaceField.setLayoutY(10);
+		characterRaceField.setPromptText("Race");
+		characterRaceField.setLayoutX(270);
+		characterRaceField.setLayoutY(72);
+		characterRaceField.setPrefWidth(100);
 		
-		thePane.getChildren().addAll(characterNameField,characterRaceField);
+		TextField charAlign = new TextField();
+		charAlign.setPromptText("Alignment");
+		charAlign.setLayoutX(370);
+		charAlign.setLayoutY(72);
+		charAlign.setPrefWidth(100);
+		
+		TextField charEXP = new TextField();
+		charEXP.setPromptText("Exp/Milestone");
+		charEXP.setLayoutX(470);
+		charEXP.setLayoutY(72);
+		charEXP.setPrefWidth(100);
+		
+		TextField charClassLevel = new TextField();
+		charClassLevel.setPromptText("Class & Level");
+		charClassLevel.setLayoutX(270);
+		charClassLevel.setLayoutY(47);
+		charClassLevel.setPrefWidth(100);
+		
+		TextField charBG = new TextField();
+		charBG.setPromptText("Background");
+		charBG.setLayoutX(370);
+		charBG.setLayoutY(47);
+		charBG.setPrefWidth(100);
+		
+		TextField playerName = new TextField();
+		playerName.setPromptText("Player Name");
+		playerName.setLayoutX(470);
+		playerName.setLayoutY(47);
+		playerName.setPrefWidth(100);
+		
+		TextField str = new TextField();
+		str.setPromptText("Str");
+		str.setLayoutX(40);
+		str.setLayoutY(155);
+		str.setPrefWidth(35);
+		
+		TextField dex = new TextField();
+		dex.setPromptText("Dex");
+		dex.setLayoutX(40);
+		dex.setLayoutY(226);
+		dex.setPrefWidth(35);
+		
+		TextField con = new TextField();
+		con.setPromptText("Con");
+		con.setLayoutX(40);
+		con.setLayoutY(298);
+		con.setPrefWidth(35);
+		
+		TextField intel = new TextField();
+		intel.setPromptText("Int");
+		intel.setLayoutX(40);
+		intel.setLayoutY(370);
+		intel.setPrefWidth(35);
+		
+		TextField wis = new TextField();
+		wis.setPromptText("Wis");
+		wis.setLayoutX(40);
+		wis.setLayoutY(442);
+		wis.setPrefWidth(35);
+		
+		TextField chr = new TextField();
+		chr.setPromptText("Cha");
+		chr.setLayoutX(40);
+		chr.setLayoutY(514);
+		chr.setPrefWidth(35);
+		
+		TextField armClass = new TextField();
+		armClass.setPromptText("AC");
+		armClass.setLayoutX(232);
+		armClass.setLayoutY(142);
+		armClass.setPrefWidth(30);
+		
+		
+		thePane.getChildren().addAll(viewSheet,characterNameField,characterRaceField,charClassLevel,charAlign,charEXP,charBG,playerName,str,dex,con,intel,wis,chr,armClass);
 		primaryStage.setScene(theScene);
+		primaryStage.sizeToScene();
 		primaryStage.show();
 	}
 }
