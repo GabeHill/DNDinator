0d60344b358a9d02543612a0835afedfde1d88ba
owner <owner@archlinux>
2018-03-06 15:16:19 -0700

    Add constructor and roll method to Die.java

--- src/models/Die.java
+++ src/models/Die.java
@@ -1,6 +1,14 @@
 package models;
 
-public class Die 
-{
+public class Die {
 	private final int numSides;
+
+	public Die(int numSides) {
+		this.numSides = numSides;
+	}
+
+	public int roll() {
+		return (int) (Math.random() * numSides) + 1;
+	}
+
 }
