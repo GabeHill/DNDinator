23f7793281c2a11bafdae60de52dc34d0967fb64
owner <owner@archlinux>
2018-03-05 22:40:07 -0700

    Update GameData constructor

--- src/logic/DungeonMasterUIController.java
+++ src/logic/DungeonMasterUIController.java
@@ -3,16 +3,31 @@ package logic;
 import models.GameData;
 
 public class DungeonMasterUIController extends PlayerUIController implements iMenu.IMenu {
-	private GameData options = new GameData();
+	private GameData options;
+
+	private void createGame(String name) {
+		options = new GameData(name);
+	}
 
 	@Override
 	public void menu() {
 		// TODO Auto-generated method stub
 
+		switch (0) {
+		case 1:
+			setRules();
+			break;
+		case 2:
+			createGame(null);
+			break;
+		default:
+			break;
+		}
+
 	}
 
 	private void setRules() {
-		options.addRule(null,null);
+		options.addRule(null, null);
 	}
 
 }
