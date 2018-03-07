bfa0bf9112e9eed850f990274e28f1c2c39f9125
AliceMcClain <36777803+AliceMcClain@users.noreply.github.com>
2018-03-06 19:55:55 -0700

    COMMMITMENT ISSUES

--- src/view/Show.java
+++ src/view/Show.java
@@ -7,6 +7,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import models.CharSheet;
 
 public class Show
 {
@@ -145,6 +146,11 @@ public class Show
 	public String getEquipment() 
 	{
 		return equipment.getText();
+	}
+	
+	public String getArmClass()
+	{
+		return armClass.getText();
 	}
 
 	public String getSpd() 
@@ -405,6 +411,251 @@ public class Show
 		{
 			attackDamage[i] = new TextField();
 			attackDamage[i].setPromptText("Atk Damage");
+			attackDamage[i].setLayoutX(322);
+			attackDamage[i].setPrefWidth(70);
+			attackDamage[i].setLayoutY(atkDmgY);
+			atkDmgY += 25;
+		}
+		
+		thePane.getChildren().addAll(viewSheet,characterNameField,characterRaceField,charClassLevel,charAlign,charEXP,charBG,playerName,str,dex,con,intel,wis,chr,armClass,spd, initiative,hp,hd,profBns,tempHp,perTraits,perIdeals,perBonds,perFlaws,additionalTraits, otherPnL,equipment);
+		
+		//adds each individual TextField[] inside of attack box to screen, as the arrays are all the same size as each other
+		for(int i = 0; i < attackNames.length; i++)
+		{
+			thePane.getChildren().addAll(attackNames[i],attackBonus[i],attackDamage[i]);
+		}
+		
+		//adds TextField[] currency to screen as it is smaller than the other arrays
+		for(int i = 0; i < currency.length; i++)
+		{
+			thePane.getChildren().add(currency[i]);
+		}
+		primaryStage.setScene(theScene);
+		primaryStage.sizeToScene();
+		primaryStage.show();
+		
+	}
+	
+	public void displayFilledCharacterSheet(Stage primaryStage, CharSheet c, boolean canEdit)
+	{
+		ImageView viewSheet = new ImageView();
+		viewSheet.setImage(new Image("file:Character Sheet (Official) - Copy_Page_1.png",790,790,true,true));
+		
+		characterNameField = new TextField();
+		characterNameField.setEditable(canEdit);
+		characterNameField.setLayoutY(59);
+		characterNameField.setLayoutX(50);
+		
+		characterRaceField = new TextField();
+		characterNameField.setEditable(canEdit);
+		characterRaceField.setLayoutX(270);
+		characterRaceField.setLayoutY(72);
+		characterRaceField.setPrefWidth(100);
+		
+		charAlign = new TextField();
+		characterNameField.setEditable(canEdit);
+		charAlign.setLayoutX(370);
+		charAlign.setLayoutY(72);
+		charAlign.setPrefWidth(100);
+		
+		charEXP = new TextField();
+		characterNameField.setEditable(canEdit);
+		charEXP.setLayoutX(470);
+		charEXP.setLayoutY(72);
+		charEXP.setPrefWidth(100);
+		
+		charClassLevel = new TextField();
+		characterNameField.setEditable(canEdit);
+		charClassLevel.setLayoutX(270);
+		charClassLevel.setLayoutY(47);
+		charClassLevel.setPrefWidth(100);
+		
+		charBG = new TextField();
+		characterNameField.setEditable(canEdit);
+		charBG.setLayoutX(370);
+		charBG.setLayoutY(47);
+		charBG.setPrefWidth(100);
+		
+		playerName = new TextField();
+		characterNameField.setEditable(canEdit);
+		playerName.setLayoutX(470);
+		playerName.setLayoutY(47);
+		playerName.setPrefWidth(100);
+		
+		perTraits = new TextField();
+		characterNameField.setEditable(canEdit);
+		perTraits.setLayoutX(420);
+		perTraits.setLayoutY(140);
+		perTraits.setPrefHeight(55);
+		
+		perIdeals = new TextField();
+		characterNameField.setEditable(canEdit);
+		perIdeals.setLayoutX(420);
+		perIdeals.setLayoutY(207);
+		perIdeals.setPrefHeight(45);
+		
+		perBonds = new TextField();
+		characterNameField.setEditable(canEdit);
+		perBonds.setLayoutX(420);
+		perBonds.setLayoutY(264);
+		perBonds.setPrefHeight(45);
+		
+		perFlaws = new TextField();
+		characterNameField.setEditable(canEdit);
+		perFlaws.setLayoutX(420);
+		perFlaws.setLayoutY(321);
+		perFlaws.setPrefHeight(45);
+		
+		additionalTraits = new TextField();
+		characterNameField.setEditable(canEdit);
+		additionalTraits.setLayoutX(410);
+		additionalTraits.setLayoutY(380);
+		additionalTraits.setPrefHeight(382);
+		additionalTraits.setPrefWidth(170);
+		
+		profBns = new TextField();
+		characterNameField.setEditable(canEdit);
+		profBns.setLayoutX(90);
+		profBns.setLayoutY(165);
+		profBns.setPrefWidth(35);
+		
+		str = new TextField();
+		characterNameField.setEditable(canEdit);
+		str.setLayoutX(40);
+		str.setLayoutY(155);
+		str.setPrefWidth(35);
+		
+		
+		dex = new TextField();
+		characterNameField.setEditable(canEdit);
+		dex.setLayoutX(40);
+		dex.setLayoutY(226);
+		dex.setPrefWidth(35);
+		
+		con = new TextField();
+		characterNameField.setEditable(canEdit);
+		con.setLayoutX(40);
+		con.setLayoutY(298);
+		con.setPrefWidth(35);
+		
+		intel = new TextField();
+		characterNameField.setEditable(canEdit);
+		intel.setLayoutX(40);
+		intel.setLayoutY(370);
+		intel.setPrefWidth(35);
+		
+		wis = new TextField();
+		characterNameField.setEditable(canEdit);
+		wis.setLayoutX(40);
+		wis.setLayoutY(442);
+		wis.setPrefWidth(35);
+		
+		chr = new TextField();
+		characterNameField.setEditable(canEdit);
+		chr.setLayoutX(40);
+		chr.setLayoutY(514);
+		chr.setPrefWidth(35);
+		
+		otherPnL = new TextField();
+		characterNameField.setEditable(canEdit);
+		otherPnL.setLayoutX(35);
+		otherPnL.setLayoutY(625);
+		otherPnL.setPrefHeight(138);
+		otherPnL.setPrefWidth(165);
+		
+		equipment = new TextField();
+		characterNameField.setEditable(canEdit);
+		equipment.setLayoutX(268);
+		equipment.setLayoutY(590);
+		equipment.setPrefHeight(173);
+		equipment.setPrefWidth(115);
+		
+		
+		armClass = new TextField();
+		characterNameField.setEditable(canEdit);
+		armClass.setLayoutX(232);
+		armClass.setLayoutY(142);
+		armClass.setPrefWidth(30);
+		
+		spd = new TextField();
+		characterNameField.setEditable(canEdit);
+		spd.setLayoutX(345);
+		spd.setLayoutY(142);
+		spd.setPrefWidth(35);
+		
+		initiative = new TextField();
+		characterNameField.setEditable(canEdit);
+		initiative.setLayoutX(285);
+		initiative.setLayoutY(142);
+		initiative.setPrefWidth(35);
+		
+		hp = new TextField();
+		characterNameField.setEditable(canEdit);
+		hp.setLayoutX(230);
+		hp.setLayoutY(200);
+		
+		tempHp = new TextField();
+		characterNameField.setEditable(canEdit);
+		tempHp.setLayoutX(230);
+		tempHp.setLayoutY(270);
+		
+		hd = new TextField();
+		characterNameField.setEditable(canEdit);
+		hd.setLayoutX(230);
+		hd.setLayoutY(320);
+		hd.setPrefWidth(65);
+		
+		
+		//Sets an array for creating textfields within the equipment box for "currency" column
+		//230 x, 590y (+30 for each iteration), 25 prefWidth 
+		currency = new TextField[5];
+		int currencyY = 590;
+		for(int i = 0; i < currency.length; i++)
+		{
+			currency[i] = new TextField();
+			characterNameField.setEditable(canEdit);
+			currency[i].setLayoutX(220);
+			currency[i].setLayoutY(currencyY);
+			currency[i].setPrefWidth(47);
+			currencyY += 26;
+		}
+		
+		//Sets an array for creating textfields within the attack box for "attack names" column
+		//220 x, 385 y (+25 for each iteration), 70 prefWidth 
+		attackNames = new TextField[7];
+		int atkNamesY = 385;
+		for(int i = 0; i < attackNames.length; i++)
+		{
+			attackNames[i] = new TextField();
+			characterNameField.setEditable(canEdit);
+			attackNames[i].setLayoutX(220);
+			attackNames[i].setPrefWidth(70);
+			attackNames[i].setLayoutY(atkNamesY);
+			atkNamesY += 25;
+		}
+		
+		//Sets an array for creating textfields within the attack box for "attack bonus" column
+		//290 x, 385 y (+25 for each iteration), 32 prefWidth 
+		attackBonus = new TextField[7];
+		int atkBnsY = 385;
+		for(int i = 0; i < attackBonus.length; i++)
+		{
+			attackBonus[i] = new TextField();
+			characterNameField.setEditable(canEdit);
+			attackBonus[i].setLayoutX(290);
+			attackBonus[i].setPrefWidth(32);
+			attackBonus[i].setLayoutY(atkBnsY);
+			atkBnsY += 25;
+		}
+		
+		//Sets an array for creating textfields within the attack box for "attack damage" column
+		//322 x, 385 y (+25 for each iteration), 70 prefWidth 
+		attackDamage = new TextField[7];
+		int atkDmgY = 385;
+		for(int i = 0; i < attackDamage.length; i++)
+		{
+			attackDamage[i] = new TextField();
+			characterNameField.setEditable(canEdit);
 			attackDamage[i].setLayoutX(322);
 			attackDamage[i].setPrefWidth(70);
 			attackDamage[i].setLayoutY(atkDmgY);
