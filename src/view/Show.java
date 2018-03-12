9254b3946ffb90e128e6677564acd523c035509c
Spencer <SSoutham@student.neumont.edu>
2018-03-12 06:10:57 -0600

    resolve conflict

--- src/view/Show.java
+++ src/view/Show.java
@@ -934,11 +934,7 @@ public class Show {
 	public int[] getCurrency() {
 		int[] charCurrency = new int[5];
 		for (int i = 0; i < charCurrency.length; i++) {
-<<<<<<< HEAD
 			charCurrency[i] = Integer.parseInt(currency[i].getText());
-=======
-			charCurrency[i] = emptyCheck(currency[i].toString());
->>>>>>> branch 'master' of https://github.com/GabeHill/DNDinator.git
 		}
 		return charCurrency;
 	}
